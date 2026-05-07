4d1828d66a3e6c8a07ffc91ffab8e2744ad77b8d
meepodeep <nbltmepodep@gmail.com>
2026-05-07 01:57:57 -0400

    fixed code

--- sketch_apr10a/sketch_apr10a.ino
+++ sketch_apr10a/sketch_apr10a.ino
@@ -10,23 +10,35 @@ void setup() {
   pinMode(buttonPin[3], INPUT_PULLUP);
   pinMode(buttonPin[4], INPUT_PULLUP);
   pinMode(buttonPin[5], INPUT_PULLUP);
+  pinMode(buttonPin[6], INPUT_PULLUP);
   Keyboard.begin();
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int buttonState[] = {digitalRead(buttonPin[0]),digitalRead(buttonPin[1]),digitalRead(buttonPin[2]),digitalRead(buttonPin[3]),digitalRead(buttonPin[4]),digitalRead(buttonPin[5])};
+  int buttonState[] = {digitalRead(buttonPin[0]),digitalRead(buttonPin[1]),digitalRead(buttonPin[2]),digitalRead(buttonPin[3]),digitalRead(buttonPin[4]),digitalRead(buttonPin[5]),digitalRead(buttonPin[6])};
     // If the button is pressed (goes from HIGH to LOW)
-  for(int i = 0; i <8; i++){
+  for(int i = 0; i <7; i++){
     if (buttonState[i] == LOW && previousButtonState[i] == HIGH) {
-      Keyboard.press(KEY_F1 + (i + 12));          
+      switch (i) {
+    case 0: Keyboard.press(KEY_F14); break;
+    case 1:  Keyboard.press(KEY_F15);  break;
+    case 2:  Keyboard.press(KEY_F16);  break;
+    case 3:  Keyboard.press(KEY_F17);  break;
+    case 4:  Keyboard.press(KEY_F18);  break;
+    case 5:  Keyboard.press(KEY_F19);  break;
+    case 6:  Keyboard.press(KEY_F20);  break;
+    default: 
+      // Do nothing if the number is out of range
+      return; 
+      }    
       delay(50);                       
     }
 
   // If the button is released (goes from LOW to HIGH)
     if (buttonState[i] == HIGH && previousButtonState[i] == LOW) {
-    Keyboard.release(KEY_F1 + (i + 12));
+     Keyboard.releaseAll();
     delay(50);
     }
 
